6c399f04f4e20775ecae4db93398ab7d51a9d763
Udbhavsomani <udbhavsomani18@gmail.com>
2023-02-18 04:16:02 +0530

    Added JSON parsing for request.data property

--- django/http/request.py
+++ django/http/request.py
@@ -1,5 +1,6 @@
 import codecs
 import copy
+import json
 from io import BytesIO
 from itertools import chain
 from urllib.parse import parse_qsl, quote, urlencode, urljoin, urlsplit
@@ -390,6 +391,11 @@ class HttpRequest:
         elif self.content_type == "application/x-www-form-urlencoded":
             self._post, self._files = (
                 QueryDict(self.body, encoding=self._encoding),
+                MultiValueDict(),
+            )
+        elif self.content_type == "application/json":
+            self._post, self._files = (
+                QueryDict(urlencode(json.loads(self.body)), encoding=self._encoding),
                 MultiValueDict(),
             )
         else:
